625ebe0407d36b43a6c8832776f69a124306b81e
Shafayathub <shafayat.ms@gmail.com>
2025-02-23 01:02:20 +0600

    dfs example on 2D grid

---
+++ dfs_on_2d_grid.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char grid[105][105];
+bool vis[105][105];
+vector<pair<int, int>> v = {{-1,0},{1,0},{0,-1},{0,1}};
+
+int n, m;
+
+bool valid(int i, int j){
+    if(i<0 || i>=n || j<0 || j>=m){
+        return false;
+    }
+    return true;
+}
+
+void dfs(int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+    for(int i = 0; i < 4; i++){
+        int ci, cj;
+        ci = si + v[i].first;
+        cj = sj + v[i].second;
+        // cout << ci << " " << cj << endl;
+        if(valid(ci,cj) && !vis[ci][cj]){
+            dfs(ci,cj);
+        }
+    }
+}
+
+int main()
+{
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+    // for(int i = 0; i<n; i++){
+    //     for (int j = 0; j<m; j++){
+    //         cout << grid[i][j];
+    //     }
+    //     cout << endl;
+    // }
+
+    int si, sj;
+    cin >> si >> sj;
+    memset(vis, false, sizeof(vis));
+    dfs(si, sj);
+
+    return 0;
+}
